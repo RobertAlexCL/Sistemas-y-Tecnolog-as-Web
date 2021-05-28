f9535226b67f6fcce76c8f82b0a2069a0581714d
RobertAlexCL <rsstocast@gmail.com>
2021-05-28 01:29:25 -0600

    changes

---
+++ Laboratorios/Lab05RC/myscript.js
@@ -0,0 +1,53 @@
+/*
+// Let vae and const
+let myName = "Roberto";
+var iLike = "Pizza";
+const PI = 3.141;
+
+// No podemos renombrar conts mientras var y let si
+// const PI = 34; tiraria un error
+
+// Tipos de datos
+
+let string = "This is a normal string";
+let number = 42;
+let bool = false;
+let array = [1, 2 ,3 ,4 ,5, "a", "b.", "c"];
+let _undefined = undefined;
+let _null = null;
+let _NaN = NaN;
+
+let object = {
+    "keyname":"value"
+};
+
+// object["keyname"] = keyname="value" entonces seria object.keyname
+*/
+/*
+function myfunction(myString, myString2=0){
+    //All of your code
+    // Acceder a la consola
+    console.log(myString);
+    console.log(myString2)
+}
+myfunction("xddd", "one")
+myfuntcion("hola")
+//La primera mostrara los parametros y la segunda mosrara el hola y el valor predeterminado que es 0
+
+
+function myfunction(myString, myString2=0){
+    //All of your code
+
+    return otrafuncion(){
+        console.log("Hola soy otra funcion");
+    }
+   
+    console.log(myString);
+    console.log(myString2)
+}
+//se corre unicamente el return, al menos que se ponga al final
+let resultado = myfunction(1234,56778);
+
+console.log(resultado);
+*/
+
